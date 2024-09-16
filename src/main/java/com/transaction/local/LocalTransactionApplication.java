8e49b38fd6cbf72803d4542836a0586b713b1c80
Shivmohan <nshivmohan08@gmail.com>
2024-09-16 15:03:21 +0530

    local txn handling init

---
+++ src/main/java/com/transaction/local/LocalTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.transaction.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transaction/local/LocalTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transaction.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
